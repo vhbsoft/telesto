8e05df433de3ff64d9ff08d669fad033fda5621a
Aren Boghozian <aren.mboghozian@gmail.com>
2017-11-21 13:04:59 -0800

    Drr configured with 5 queues

---
+++ deficit-round-robin_5/helper/deficit-round-robin-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef DEFICIT_ROUND_ROBIN_HELPER_H
+#define DEFICIT_ROUND_ROBIN_HELPER_H
+
+#include "ns3/deficit-round-robin.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* DEFICIT_ROUND_ROBIN_HELPER_H */
+
